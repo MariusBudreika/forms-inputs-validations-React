3a6117c3d1ca422b2cd0b6e52398084d157cf1d8
Marius <marius.budreika09@gmail.com>
2022-07-21 16:53:50 +0300

    added simple name validation with feedback

--- src/components/Input.js
+++ src/components/Input.js
@@ -4,6 +4,7 @@ import styles from "./Input.module.scss";
 
 const Input = () => {
   const [enteredName, setEnteredName] = useState("");
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
@@ -11,20 +12,33 @@ const Input = () => {
 
   const formSubmisionHandler = (event) => {
     event.preventDefault();
+
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+      return;
+    }
+
+    setEnteredNameIsValid(true);
     console.log(enteredName);
     setEnteredName("");
   };
+
+  const nameInputClasses = enteredNameIsValid ? "" : styles.controlInvalid;
 
   return (
     <form onSubmit={formSubmisionHandler} className={styles.container}>
       <div className={styles.control}>
         <label htmlFor="">Your name:</label>
         <input
+          className={nameInputClasses}
           value={enteredName}
           type="text"
           id="name"
           onChange={nameInputChangeHandler}
         />
+        {!enteredNameIsValid && (
+          <p className={styles.error}>Name must not be emty.</p>
+        )}
       </div>
       <div className={styles.actions}>
         <button>Submit</button>
