c7628625663dd0eb71092f1f294b644db4de84cd
Marius <marius.budreika09@gmail.com>
2022-07-21 16:27:39 +0300

    added event handlers

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styles from "./Input.module.scss";
 
 const Input = () => {
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmisionHandler = (event) => {
+    event.preventDefault();
+    console.log(enteredName);
+    setEnteredName("");
+  };
+
   return (
-    <form action="" className={styles.container}>
+    <form onSubmit={formSubmisionHandler} className={styles.container}>
       <div className={styles.control}>
         <label htmlFor="">Your name:</label>
-        <input type="text" />
+        <input
+          value={enteredName}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+        />
       </div>
       <div className={styles.actions}>
         <button>Submit</button>
